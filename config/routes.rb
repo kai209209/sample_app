fa543913f9debdb41e2eb1db831ab458f7bddcc4
kai209209 <361561797@qq.com>
2013-10-24 20:13:16 +0800

    Finish user edit ,update,index,adm destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ SampleApp::Application.routes.draw do
   match '/signup', to:'users#new',via:'get'
   match '/signin', to:'sessions#new', via:'get'
   match '/signout', to:'sessions#destroy', via:'delete'
+#  match '/edit', to:'users#edit', via:'update'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
