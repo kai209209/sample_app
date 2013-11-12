2d8e2548a59a38c717b90fdf15cd5bb1467aef1f
kai209209 <361561797@qq.com>
2013-11-12 18:05:42 +0800

    add follower user-part

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,58 +1,58 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only:[:index,:edit,:update,:destroy]
-  before_action :correct_user, only:[:edit,:update]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
 
   def index
-    @users=User.paginate(page:params[:page])
+    @users = User.paginate(page:params[:page])
   end
 
   def edit
   end
 
   def update
-   if @user.update_attributes(user_params)
-     flash[:success]="Profile updated"
+    if @user.update_attributes(user_params)
+     flash[:success] = "Profile updated"
      sign_in @user
      redirect_to @user
-   else
-    render 'edit'
-   end
+    else
+     render 'edit'
+    end
   end
 
   def show
-     @user=User.find(params[:id])
-     @microposts=@user.microposts.paginate(page: params[:page])
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
-     @user=User.new
+    @user = User.new
   end
 
   def create
-     @user=User.new(user_params)
+    @user = User.new(user_params)
      if @user.save
-        sign_in @user
-        flash[:success]="Welcome to the Sample App!"
-	redirect_to @user
+       sign_in @user
+       flash[:success] = "Welcome to the Sample App!"
+	     redirect_to @user
      else
-      render 'new'
-  end
+       render 'new'
+      end
   end
   
   def destroy
     User.find(params[:id]).destroy
-    flash[:success]="User destroyed."
+    flash[:success] = "User destroyed."
     redirect_to users_url
   end
 
   private
      def user_params
-       params.require(:user).permit(:name,:email,:password,:password_confirmation)
+       params.require(:user).permit(:name, :email, :password, :password_confirmation)
      end
 
      def correct_user
-       @user=User.find(params[:id])
+       @user = User.find(params[:id])
        redirect_to(root_path) unless current_user?(@user)
      end
 
--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -1,10 +1,17 @@
-namespace :db do
-  desc "Fill database with sample data"
-  task populate: :environment do
-    admin = User.create!(name: "Example User",
-                email: "example@railstutorial.org",
-               password: "foobar",
-               password_confirmation: "foobar",
+ namespace :db do
+   desc "Fill database with sample data"
+   task populate: :environment do
+     make_users
+     make_microposts
+     make_relationships
+   end
+ end
+
+ def make_users
+    admin = User.create!(name: "admin",
+                email: "admin@admin.com",
+               password: "123456",
+               password_confirmation: "123456",
                admin: true )
    99.times do |n|
       name =Faker::Name.name
@@ -15,10 +22,21 @@ namespace :db do
                    password: password,
                    password_confirmation: password )
      end
-     users=User.all(limit:6)
-     50.times do
-       content =Faker::Lorem.sentence(5)
-       users.each {|user| user.microposts.create!(content: content)}
+ end
+
+ def make_microposts
+   users = User.all(limit:6)
+   50.times do
+     content = Faker::Lorem.sentence(5)
+     users.each { |user| user.microposts.create!(content: content) }
     end
   end
-end
+
+ def make_relationships
+   users = User.all
+   user = users.first
+   followed_users = users[2..50]
+   followers = users[3..40]
+   followed_users.each { |followed| user.follow!(followed) }
+   followers.each { |follower| follower.follow!(user) }
+ end
