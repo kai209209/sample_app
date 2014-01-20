b3c806fafe732e82627da7e6052ac0e9bf5d0837
kai209209 <361561797@qq.com>
2014-01-20 15:41:19 +0800

    add notifications

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,12 +3,20 @@ class Activity < ActiveRecord::Base
   belongs_to :trackable, polymorphic: true
   has_many :notifications
 
-  def self.find_unread_activities current_user
+  def self.find_activities current_user
     activity_ids = current_user.notifications.map(&:activity_id)
-    notifications = current_user.notifications.where(read: false)
-    notifications.update_all(read: true)
     activities = Activity.order("id desc")
     activities = activities.where(id: activity_ids)
     activities
+  end
+ 
+  def select_class current_user
+    notice = notifications.where(user_id: current_user.id ).first
+    if notice.read 
+      "class1" 
+    else
+      notice.update(read: true)
+      "class2"
+    end
   end
 end
